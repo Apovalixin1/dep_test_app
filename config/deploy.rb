d6d1a3297aa052f08d0ec2c0a2d800412d6f1a93
Anton <4spam.apovalixin@bk.ru>
2025-03-31 17:05:34 +0300

    Добавлено использование sudo в конфигурации деплоя.

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,5 @@ set :repo_url, "https://github.com/Apovalixin1/dep_test_app"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :use_sudo, true
